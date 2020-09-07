42a61aaf1326cd616c9075d5684aefe947067cf6
Zhengyuan Ma <textangel@gmail.com>
2020-09-07 22:30:15 +0000

    attempting to base64 encode image as dataURI and send it in POST request. Modules for encoding as DataURI must be implemented first, and then strung together with code for  POST which I copied to the end of the file assets/projects/image_style_transfer/image_style_transfer.js

--- assets/projects/image_style_transfer/image_style_transfer.js
+++ assets/projects/image_style_transfer/image_style_transfer.js
@@ -7,6 +7,7 @@ function display_image(canvas_name) {
     var imgcanvas = document.getElementById(img_canvas_id);
     var fileinput = document.getElementById(img_input_id);
     console.log(imgcanvas, fileinput)
+    
     imgcanvas.src = window.URL.createObjectURL(fileinput.files[0])
     imgcanvas.style.display="inline"
 }
@@ -25,11 +26,9 @@ function upload_images(){
     var dir_id = Math.random().toString(36).substring(2, 15); //Random uid
     if (base_img.src && style_img.src && base_img.src.length > 1 && style_img.src.length > 1){
         $error_div.text("\xa0")
-        console.log("beginning upload")
         var base_img_data = document.getElementById("finput_base").files[0];
         var style_img_data = document.getElementById("finput_style").files[0];
-        image_upload(base_img_data, "base", dir_id)
-        image_upload(style_img_data, "style", dir_id)
+        uploadImage(base_img.src, base_img_data.type, style_img.src, style_img_data.type)
     } else {
         if ($error_div.text() == error_msg){
             $error_div.text("\xa0")
@@ -62,3 +61,75 @@ function upload_images(){
 //         console.log(directory_name + "/" + image_name + " uploaded.");
 //     });
 // }
+
+// This function accepts three arguments, the URL of the image to be 
+// converted, the mime type of the Base64 image to be output, and a 
+// callback function that will be called with the data URL as its argument 
+// once processing is complete
+
+var convertToBase64 = function(url, imagetype, callback){
+
+    var img = document.createElement('IMG'),
+        canvas = document.createElement('CANVAS'),
+        ctx = canvas.getContext('2d'),
+        data = '';
+
+    // Set the crossOrigin property of the image element to 'Anonymous',
+    // allowing us to load images from other domains so long as that domain 
+    // has cross-origin headers properly set
+
+    img.crossOrigin = 'Anonymous'
+
+    // Because image loading is asynchronous, we define an event listening function that will be called when the image has been loaded
+    img.onLoad = function(){
+        // When the image is loaded, this function is called with the image object as its context or 'this' value
+        canvas.height = this.height;
+        canvas.width = this.width;
+        ctx.drawImage(this, 0, 0);
+        data = canvas.toDataURL(imagetype);
+        console.log("img_data: ", data)
+        callback(data);
+    };
+
+    // We set the source of the image tag to start loading its data. We define 
+    // the event listener first, so that if the image has already been loaded 
+    // on the page or is cached the event listener will still fire
+
+    img.src = url;
+};
+
+// Here we define the function that will send the request to the server. 
+
+var sendBase64ToServer = function(base64, tag){
+    var httpPost = new XMLHttpRequest(),
+        path = 'https://api.zhengyuanma.us/test_api/style_transfer_test/image_upload',
+        data = JSON.stringify({tag: base64});
+        console.log("Sending ", data)
+    httpPost.onreadystatechange = function(err) {
+            if (httpPost.readyState == 4 && httpPost.status == 200){
+                console.log(httpPost.responseText);
+            } else {
+                console.log(err);
+            }
+        };
+    // Set the content type of the request to json since that's what's being sent
+    httpPost.setHeader('Content-Type', 'application/json');
+    httpPost.open("POST", path, true);
+    httpPost.send(data);
+};
+
+// This wrapper function will accept the name of the image, the url, and the 
+// image type and perform the request
+
+var uploadImage = function(base_src, base_type, style_src, style_type){
+    console.log("beginning upload")
+    convertToBase64(base_src, base_type, function(data){
+        sendBase64ToServer(data, "base_img");
+    });
+    convertToBase64(style_src, style_type, function(data){
+        sendBase64ToServer(data, "style_img");
+    });
+};
+
+// Call the function with the provided values. The mime type could also be png
+// or webp
