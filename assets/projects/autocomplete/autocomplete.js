bc1e444d0a40c4b62f1b975c3fc27ec918a6569a
Zhengyuan Ma <textangel@gmail.com>
2020-09-09 01:25:39 +0000

    Added lots of comments to autocomplete.js. Still not done commenting

--- assets/projects/autocomplete/autocomplete.js
+++ assets/projects/autocomplete/autocomplete.js
@@ -1,19 +1,196 @@
 /*
-    https://www.youtube.com/watch?v=uaa9HVC-tQA
-    Define Variables
-    Focus the Input
+    File: autocomplete.js
+    This file contains the code for the GPT-2 Autocomplete Web App for zhengyuanma.us
+
+    All globals have prefix `acp_` to avoid clashing with other files.
+    Inspiration from: https://www.youtube.com/watch?v=uaa9HVC-tQA
 */
 
-// Create placeholder input to serve as background
-var $acp_input_box = $('#project1_box'),
-    $acp_background_box = $('#project1_box2'),
+/* 1. Variables and Setup*/
+var $acp_input_box = $('#project1_box'), // The box that the user types into
+    $acp_background_box = $('#project1_box2'), // The box that displays the proposed word. It is behind the input box, and a different color, so the user sees them as one box.
+    acp_authors_category_list_path = "./assets/projects/autocomplete/authors_category.json",
+    acp_max_input_lines_to_send_to_api = 10;
     acp_matches = [],
-    acp_all_authors = null,
-    cur_author_string = "",
-    punct_delimiter_codes = [32, 16, 188, 190, 186, 191] //space ! , . : ; ?
-    $temp = null;
-    inside_tab_processing = false;
-    invisible_char = "\ufeff";
+    acp_all_authors = null, // The list of all possible authors and author categories, to be loaded from file. See  `2. Updating the GPT2 Author Mode`
+    acp_gpt2_author_id_string = "",
+    acp_punct_delimiter_codes = [32, 16, 188, 190, 186, 191] //space ! , . : ; ?
+    acp_inside_tab_processing = false;
+    acp_invisible_char = "\ufeff";
+
+
+/* 2. Updating the GPT2 Author Mode
+
+    GPT2 changes author modes by sending each line with a prefix which lets it know which author is currently active.
+    That is, instead of 
+         `Hello, dear man! What is this? I have long felt that the true philosopher is not in himself, that he is one who can speak truth without being...`
+    The input to GPT2 is
+         `B| Friedrich Wilhelm Nietzsche ??>>> Hello, dear man! What is this? I have long felt that the true philosopher
+          B| Friedrich Wilhelm Nietzsche ??>>> is not in himself, that he is one who can speak truth without being`
+    The prefix `B| Friedrich Wilhelm Nietzsche ??>>>` is called the `acp_gpt2_author_id_string`, and it determines the author mode.
+    In this section, we allow the user to pick from a list of available authors in a lookup field, and update the mode if the user picks an author.
+ */
+
+ /* 2.1 Helper Functions - Update Author Mode.
+    These functions allow us to update the current author mode by updating the `acp_gpt2_author_id_string`.
+    Note that we do not make use of the author categories in this case
+    The author category is selected randomly from avaialble ones given the author.
+*/
+function updateCurrentAuthor(author_name, author_cat){
+    acp_gpt2_author_id_string = author_cat + "| "+author_name+" ??>>>"
+    $('#authorDisplay p b').text(author_name)
+}
+function updateCurrentAuthorRandomCat(author_name){
+    possible_cats = acp_all_authors[author_name]
+    selected_cat = possible_cats[Math.floor(Math.random() * possible_cats.length)] //Javascript version of select random
+    updateCurrentAuthor(author_name, selected_cat)
+}
+
+/* 2.2 Load the list of all authors into the author into the author lookup field `$("#authorLookup")`
+    The lookup field makes use of `jqery-ui`'s `autocomplete` feature.
+    Initialize the app to a random author mode.
+*/
+$.getJSON(acp_authors_category_list_path, function(authors){
+    acp_all_authors = authors;
+    var authors_names = Object.keys(authors);
+    var acp_random_init_selected_author = authors_names[Math.floor(Math.random() * authors_names.length)]
+    updateCurrentAuthorRandomCat(acp_random_init_selected_author) //We initialize the author mode to a random author.
+    var $input = $("#authorLookup").autocomplete({
+        source: function(request, response) {
+            var results = $.ui.autocomplete.filter(authors_names, request.term);
+            response(results.slice(0, 10)); //Limit responses to length 10 so we don't overwhelm the user. 
+        },
+        select: function (event, ui) {
+            $(this).val(ui.item.value);
+            $('#authorLookupForm').submit();
+            event.stopPropagation() //Like .preventDefault(). Prevents user from leaving the page when clicking on selection.
+        }
+    });
+});
+
+/* 2.3 If a new author is selection from the lookup field, we set the app's author mode to the selected author.
+*/
+$('#authorLookupForm').submit(function(event){
+    event.preventDefault();
+    author_name = $('#authorLookup').val()
+    console.log(author_name)
+    if (Object.keys(acp_all_authors).includes(author_name)){
+        updateCurrentAuthorRandomCat(author_name)
+    }
+    
+})
+
+/* 3. Processing The Text and Calling the API
+    Given Some User text, we preprocess it into the format desired by the API and call the API.
+ */
+
+ /* 3.1 Processing The Text
+    As noted above, our version of GPT-2 requires the text in a certain format (dependent on our training data). In particular, instead of 
+         `Hello, dear man! What is this? I have long felt that the true philosopher is not in himself, that he is one who can speak truth without being...`
+    The input to our GPT2 should be
+         `B| Friedrich Wilhelm Nietzsche ??>>> Hello, dear man! What is this? I have long felt that the true philosopher
+          B| Friedrich Wilhelm Nietzsche ??>>> is not in himself, that he is one who can speak truth without being`
+
+    Given some user text, several preprocessing steps are necessary:
+        1. Break up long paragraphs into separate lines
+        2. Append the `acp_gpt2_author_id_string` to the beginning of each line
+ */
+function processInputTextGPT2(inputText, acp_gpt2_author_id_string, max_lines_to_keep){
+    const split_text_max_len = 70 //break up lines if longer than 70 characters
+    if (max_lines_to_keep <= 0) max_lines_to_keep = 10 //we retain the last `max_lines_to_keep` lines of user text
+    // First we split along any newlines already present in the text
+    split_Text = inputText.split("\n") 
+    final_text = []
+    for (ix in split_Text){
+        //Then for every line in the split text, if it is longer than `split_text_max_len` we
+        //split it by spaces and traverse it, adding words until the resultant line is longer than `split_text_max_len` 
+        // Note that whenever we add a new line, we append the `acp_gpt2_author_id_string` to it first.
+        if (split_Text[ix].length <= split_text_max_len)
+            final_text.push(acp_gpt2_author_id_string + " " + split_Text[ix].trim())
+        else {
+            total_len = 0
+            cur_split = []
+            space_split = split_Text[ix].split(" ")
+            for (ix2 in space_split){
+                if (total_len + space_split[ix2].length <= split_text_max_len){
+                    cur_split.push(space_split[ix2])
+                    total_len += space_split[ix2].length
+                } else {
+                    // When it is longer than `split_text_max_len`, we break at this point, join the line and add the line to our final list of lines.
+                    // Note that whenever we add a new line, we append the `acp_gpt2_author_id_string` to it first.
+                    final_text.push(acp_gpt2_author_id_string + " " + cur_split.join(" ").trim())
+                    total_len = 0
+                    cur_split = []
+                }
+            }
+            if (cur_split.length > 0)
+                final_text.push(acp_gpt2_author_id_string + " " + cur_split.join(" ").trim()) //Add back the leftover line in last iteration
+        }
+    }
+    //We only keep the last `max_lines_to_keep` lines
+    final_text = final_text.slice(Math.max(final_text.length-max_lines_to_keep, 0))
+    // We add 10 copies of the empty line (rather, the line with only `acp_gpt2_author_id_string`)
+    // This is a hack to make GPT-2 think it's at the beginning of a section, so will write fluently without interruptions.
+    for (var i=0; i<10; i++) {
+        final_text.unshift(acp_gpt2_author_id_string);        
+    }
+    // We join the list of lines back by "\n" to send to the server.
+    return final_text.join("\n")
+}
+
+ /* 3.2 Calling the API
+    The GPT2 Model is exposed on our app server at `https://api.zhengyuanma.us/api/gpt2_autocomplete/predict`
+
+ */
+function getMatchesAPI(inputText) {
+    // Preprocess the text
+    var last_tokens = processInputTextGPT2(inputText, acp_gpt2_author_id_string, acp_max_input_lines_to_send_to_api)
+    console.log(last_tokens)
+    
+    //Set Up The Request
+    var matchList = [];
+    var request = new XMLHttpRequest();
+    var url = 'https://api.zhengyuanma.us/api/gpt2_autocomplete/predict'
+    
+    //Set up the response handler
+    request.onreadystatechange = function() {
+        if(request.readyState == 4 && request.status == 200) {
+            // GPT-2 generates for probabilities for 50,000 possible next words and samples over those probabilies.
+            // The response is in JSON format and contains two fields: 
+            // - `response`: a list of size 40 most likely candidate next words, entries are tuples of (word, probability (stringified float))
+            // - `sample_token`: GPT2's recommendation for the next word. Note that this may not be in `response` as GPT-2 may have sampled from outside the top 40.
+            api_response = JSON.parse(request.responseText);
+            
+            // We first convert the stringified float back to float
+            acp_matches = api_response["response"];
+            acp_matches_ = []
+            for (ix in acp_matches)
+                acp_matches_.push([acp_matches[ix][0], parseFloat(acp_matches[ix][1])]);
+            acp_matches = acp_matches_
+            
+            // Out of the top 40 we keep the top 20 to graph
+            acp_matches_top_k = acp_matches.slice(Math.max(acp_matches.length - 20, 0))
+            acp_sample_token = api_response["sample_token"];
+            if (acp_matches.length > 0) {
+                // We write the recommended token out to the text field.
+                displaySampledMatches (acp_sample_token, acp_matches_top_k);
+                // We graph the top 20 responses
+                updateChart(acp_matches_top_k, myChart);
+            }
+        // We don't do any error checking
+        }
+        //If the user had queried the model using a `tab`, we release the lock on the `tab`. This prevents the user from spamming the model.
+        if (acp_inside_tab_processing == true)
+            acp_inside_tab_processing = false;
+    }
+    //Send The Request
+    request.open('POST', url);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(JSON.stringify({"text":last_tokens}));
+}
+
+/* 4. Autocomplete Text Input
+ */
 
 // Behavior for focus and blur to achieve the visual effect
 $acp_input_box.focus(function(){
@@ -26,14 +203,14 @@ $acp_input_box.focus(function(){
     if ($input.val() == '')
         $acp_background_box.css('color', 'transparent');
  }).keyup(delay(function(event){
-    if(punct_delimiter_codes.includes(event.keyCode)){
+    if(acp_punct_delimiter_codes.includes(event.keyCode)){
          if (this.value.length > 0)
              getMatchesAPI(this.value);
      }
  }, 200)).keydown(function(event) { 
     if (event.keyCode == 9) { //tab
-        if (!inside_tab_processing){
-            inside_tab_processing = true;
+        if (!acp_inside_tab_processing){
+            acp_inside_tab_processing = true;
             $(this).focus();
             event.preventDefault();
             var cand = $acp_background_box.val()
@@ -60,43 +237,6 @@ $acp_input_box.focus(function(){
 
 
 
-$tmp = null
-
-function getMatchesAPI(inputText) {
-    var last_tokens = processInputTextGPT2(inputText, cur_author_string, 10)
-    console.log('last_tokens', last_tokens)
-    var matchList = [];
-    var request = new XMLHttpRequest();
-    var url = 'https://api.zhengyuanma.us/api/gpt2_autocomplete/predict' //?text=' + encodeURIComponent(last_tokens);
-    
-    request.open('POST', url);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    request.send(JSON.stringify({"text":last_tokens}));
-
-    request.onreadystatechange = function() {//Call a function when the state changes.
-        if(request.readyState == 4 && request.status == 200) {
-            // console.log(request.responseText)
-            $tmp = request.responseText
-            api_response = JSON.parse(request.responseText);
-            
-            // Post-process: Convert string to int
-            acp_matches = api_response["response"];
-            acp_matches_ = []
-            for (ix in acp_matches)
-                acp_matches_.push([acp_matches[ix][0], parseFloat(acp_matches[ix][1])]);
-            acp_matches = acp_matches_
-            acp_matches_top_k = acp_matches.slice(Math.max(acp_matches.length - 20, 0))
-            acp_sample_token = api_response["sample_token"];
-            if (acp_matches.length > 0) {
-                displaySampledMatches (acp_sample_token, acp_matches_top_k);
-                updateChart(acp_matches_top_k, myChart);
-            }
-        }
-        if (inside_tab_processing == true)
-            inside_tab_processing = false;
-    }
-}
-
 function displaySampledMatches (sample_token, topk_matches) {
     var last_whitespace = $acp_input_box.val().match(/\s+$/)
     if (last_whitespace && last_whitespace[0].includes("\n")){
@@ -115,35 +255,6 @@ function displaySampledMatches (sample_token, topk_matches) {
 }
 
 
-function processInputTextGPT2(inputText, cur_author_string, num_lines){
-    if (num_lines <=0) num_lines = 10
-    const split_text_max_len = 70
-    split_Text = inputText.split("\n")
-    final_text = []
-    for (ix in split_Text){
-        if(split_Text[ix].length > split_text_max_len){
-            total_len = 0
-            cur_split = []
-            space_split = split_Text[ix].split(" ")
-            for (ix2 in space_split){
-                if (total_len + space_split[ix2].length <= split_text_max_len){
-                    cur_split.push(space_split[ix2])
-                    total_len += space_split[ix2].length
-                } else {
-                    final_text.push(cur_author_string + " " + cur_split.join(" ").trim())
-                    total_len = 0
-                    cur_split = []
-                }
-            }
-            if (cur_split.length > 0)
-                final_text.push(cur_author_string + " " + cur_split.join(" ").trim())
-        } else{
-            final_text.push(cur_author_string + " " + split_Text[ix].trim())
-        }
-    }
-    final_text = final_text.slice(Math.max(final_text.length-num_lines, 0))
-    return final_text.join("\n")
-}
 
   
 function updateChart(topKList, chart) {
@@ -153,47 +264,6 @@ function updateChart(topKList, chart) {
 }
 
 
-// Updating the Current Author
-$('#authorLookupForm').submit(function(event){
-    event.preventDefault();
-    author_name = $('#authorLookup').val()
-    console.log(author_name)
-    if (Object.keys(acp_all_authors).includes(author_name)){
-        possible_cats = acp_all_authors[author_name]
-        selected_cat = possible_cats[Math.floor(Math.random() * possible_cats.length)]
-        updateCurrentAuthor(author_name, selected_cat)
-    }
-    
-})
-
-function updateCurrentAuthor(author_name, author_cat){
-    cur_author_string = author_cat + "| "+author_name+" ??>>>"
-    $('#authorDisplay p b').text(author_name)
-}
-updateCurrentAuthor("Abraham Lincoln", "E456")
-
-
-//Dropdown many with authors
-$.getJSON("./assets/projects/autocomplete/authors_category.json", function(authors){
-    acp_all_authors = authors;
-    authors_names = Object.keys(authors);
-    var $input = $("#authorLookup").autocomplete({
-        source: function(request, response) {
-            var results = $.ui.autocomplete.filter(authors_names, request.term);
-            response(results.slice(0, 10));
-        },
-        select: function (event, ui) {
-            $(this).val(ui.item.value);
-            $('#authorLookupForm').submit();
-            event.stopPropagation()
-        }
-    });
-});
-
-
-$(".autocomplete").select(function(event){
-    event.preventDefault()
-})
 
 // Helper Functions
 function delay(callback, ms) {
@@ -205,12 +275,7 @@ function delay(callback, ms) {
         callback.apply(context, args);
       }, ms || 0);
     };
-  }
-
-function chunkString(str, length) {
-    return str.match(new RegExp('.{1,' + length + '}', 'g'));
 }
-
 
 /* Displays*/
 // CSS Styling for the Overlapping Boxes
